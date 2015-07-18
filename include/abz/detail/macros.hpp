c0dfeee94f748ef4c3d645a59e715d84d108d9bc
Pierre-Luc Perrier <pluc@the-pluc.net>
2015-07-18 19:42:30 +0200

    Add macros.hpp

---
+++ include/abz/detail/macros.hpp
@@ -0,0 +1,16 @@
+#ifndef abz_macros_hpp
+#define abz_macros_hpp
+
+/// @file macros.hpp
+/// @brief Internal macros
+
+#define ABZ_NAMESPACE_BEGIN namespace abz {
+#define ABZ_NAMESPACE_END }
+
+/// @def ABZ_NAMESPACE_BEGIN
+/// Opens the abz namespace
+
+/// @def ABZ_NAMESPACE_END
+/// Closes the abz namespace
+
+#endif // abz_macros_hpp
