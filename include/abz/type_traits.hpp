6afdc26ed825e8e2a15483958a0ab0562c498b91
Pierre-Luc Perrier <pluc-dev@the-pluc.net>
2016-11-12 15:59:52 +0100

    Add type traits variable templates from C++17

--- include/abz/type_traits.hpp
+++ include/abz/type_traits.hpp
@@ -89,6 +89,156 @@ using underlying_type_t = typename std::underlying_type<T>::type;
 template <class F, class... ArgTypes>
 using result_of_t = typename std::result_of<F(ArgTypes...)>::type;
 
+/////////////////////////////////////////////////////////////////////////////////
+// C++17 #P0006R0                                                              //
+// Adopt Type Traits Variable Templates from Library Fundamentals TS for C++17 //
+/////////////////////////////////////////////////////////////////////////////////
+
+#ifdef __cpp_variable_templates
+
+// 20.10.4.1, primary type categories
+template <class T>
+constexpr bool is_void_v = std::is_void<T>::value;
+template <class T>
+constexpr bool is_null_pointer_v = std::is_null_pointer<T>::value;
+template <class T>
+constexpr bool is_integral_v = std::is_integral<T>::value;
+template <class T>
+constexpr bool is_floating_point_v = std::is_floating_point<T>::value;
+template <class T>
+constexpr bool is_array_v = std::is_array<T>::value;
+template <class T>
+constexpr bool is_pointer_v = std::is_pointer<T>::value;
+template <class T>
+constexpr bool is_lvalue_reference_v = std::is_lvalue_reference<T>::value;
+template <class T>
+constexpr bool is_rvalue_reference_v = std::is_rvalue_reference<T>::value;
+template <class T>
+constexpr bool is_member_object_pointer_v = std::is_member_object_pointer<T>::value;
+template <class T>
+constexpr bool is_member_function_pointer_v = std::is_member_function_pointer<T>::value;
+template <class T>
+constexpr bool is_enum_v = std::is_enum<T>::value;
+template <class T>
+constexpr bool is_union_v = std::is_union<T>::value;
+template <class T>
+constexpr bool is_class_v = std::is_class<T>::value;
+template <class T>
+constexpr bool is_function_v = std::is_function<T>::value;
+
+// 20.10.4.2, composite type categories
+template <class T>
+constexpr bool is_reference_v = std::is_reference<T>::value;
+template <class T>
+constexpr bool is_arithmetic_v = std::is_arithmetic<T>::value;
+template <class T>
+constexpr bool is_fundamental_v = std::is_fundamental<T>::value;
+template <class T>
+constexpr bool is_object_v = std::is_object<T>::value;
+template <class T>
+constexpr bool is_scalar_v = std::is_scalar<T>::value;
+template <class T>
+constexpr bool is_compound_v = std::is_compound<T>::value;
+template <class T>
+constexpr bool is_member_pointer_v = std::is_member_pointer<T>::value;
+
+// 20.10.4.3, type properties
+template <class T>
+constexpr bool is_const_v = std::is_const<T>::value;
+template <class T>
+constexpr bool is_volatile_v = std::is_volatile<T>::value;
+template <class T>
+constexpr bool is_trivial_v = std::is_trivial<T>::value;
+template <class T>
+constexpr bool is_trivially_copyable_v = std::is_trivially_copyable<T>::value;
+template <class T>
+constexpr bool is_standard_layout_v = std::is_standard_layout<T>::value;
+template <class T>
+constexpr bool is_pod_v = std::is_pod<T>::value;
+template <class T>
+constexpr bool is_literal_type_v = std::is_literal_type<T>::value;
+template <class T>
+constexpr bool is_empty_v = std::is_empty<T>::value;
+template <class T>
+constexpr bool is_polymorphic_v = std::is_polymorphic<T>::value;
+template <class T>
+constexpr bool is_abstract_v = std::is_abstract<T>::value;
+template <class T>
+constexpr bool is_final_v = std::is_final<T>::value;
+template <class T>
+constexpr bool is_signed_v = std::is_signed<T>::value;
+template <class T>
+constexpr bool is_unsigned_v = std::is_unsigned<T>::value;
+template <class T, class... Args>
+constexpr bool is_constructible_v = std::is_constructible<T, Args...>::value;
+template <class T>
+constexpr bool is_default_constructible_v = std::is_default_constructible<T>::value;
+template <class T>
+constexpr bool is_copy_constructible_v = std::is_copy_constructible<T>::value;
+template <class T>
+constexpr bool is_move_constructible_v = std::is_move_constructible<T>::value;
+template <class T, class U>
+constexpr bool is_assignable_v = std::is_assignable<T, U>::value;
+template <class T>
+constexpr bool is_copy_assignable_v = std::is_copy_assignable<T>::value;
+template <class T>
+constexpr bool is_move_assignable_v = std::is_move_assignable<T>::value;
+template <class T>
+constexpr bool is_destructible_v = std::is_destructible<T>::value;
+template <class T, class... Args>
+constexpr bool is_trivially_constructible_v = std::is_trivially_constructible<T, Args...>::value;
+template <class T>
+constexpr bool is_trivially_default_constructible_v
+  = std::is_trivially_default_constructible<T>::value;
+template <class T>
+constexpr bool is_trivially_copy_constructible_v = std::is_trivially_copy_constructible<T>::value;
+template <class T>
+constexpr bool is_trivially_move_constructible_v = std::is_trivially_move_constructible<T>::value;
+template <class T, class U>
+constexpr bool is_trivially_assignable_v = std::is_trivially_assignable<T, U>::value;
+template <class T>
+constexpr bool is_trivially_copy_assignable_v = std::is_trivially_copy_assignable<T>::value;
+template <class T>
+constexpr bool is_trivially_move_assignable_v = std::is_trivially_move_assignable<T>::value;
+template <class T>
+constexpr bool is_trivially_destructible_v = std::is_trivially_destructible<T>::value;
+template <class T, class... Args>
+constexpr bool is_nothrow_constructible_v = std::is_nothrow_constructible<T, Args...>::value;
+template <class T>
+constexpr bool is_nothrow_default_constructible_v = std::is_nothrow_default_constructible<T>::value;
+template <class T>
+constexpr bool is_nothrow_copy_constructible_v = std::is_nothrow_copy_constructible<T>::value;
+template <class T>
+constexpr bool is_nothrow_move_constructible_v = std::is_nothrow_move_constructible<T>::value;
+template <class T, class U>
+constexpr bool is_nothrow_assignable_v = std::is_nothrow_assignable<T, U>::value;
+template <class T>
+constexpr bool is_nothrow_copy_assignable_v = std::is_nothrow_copy_assignable<T>::value;
+template <class T>
+constexpr bool is_nothrow_move_assignable_v = std::is_nothrow_move_assignable<T>::value;
+template <class T>
+constexpr bool is_nothrow_destructible_v = std::is_nothrow_destructible<T>::value;
+template <class T>
+constexpr bool has_virtual_destructor_v = std::has_virtual_destructor<T>::value;
+
+// See 20.10.5, type property queries
+template <class T>
+constexpr std::size_t alignment_of_v = std::alignment_of<T>::value;
+template <class T>
+constexpr std::size_t rank_v = std::rank<T>::value;
+template <class T, unsigned I = 0>
+constexpr std::size_t extent_v = std::extent<T, I>::value;
+
+// See 20.10.6, type relations
+template <class T, class U>
+constexpr bool is_same_v = std::is_same<T, U>::value;
+template <class Base, class Derived>
+constexpr bool is_base_of_v = std::is_base_of<Base, Derived>::value;
+template <class From, class To>
+constexpr bool is_convertible_v = std::is_convertible<From, To>::value;
+
+#endif // __cpp_variable_templates
+
 ABZ_NAMESPACE_END
 
 #endif // abz_type_traits_hpp
